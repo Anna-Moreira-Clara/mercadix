afa15e639d184facba9050848d5a4ed3857e0706
Anna Clara Moreira <clarapbi181@gmail.com>
2025-04-03 16:35:09 -0300

    fiz a conexão com o servidor

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require ('dotenv').config(); //dotenv -> carrega as variáveis de amibiente do a
 const express = require('express'); //express-> framework para criar o servidor e rotas da API
 const mysql = require('mysql'); //mysql2-> biblioteca para conectar ao mysql
 const cors = require('cors'); //cors-> permite que o frontend(react) se conecte ao backend sem bloqueios
+const usuariosRouter = require('./routes/usuarios');
+app.use('/usuarios',usuariosRouter);
 
 //Inicialização do Express
 const app = express(); //Inicializa o servidor
@@ -23,18 +25,18 @@ db.connect(err =>{
         return;
     }
     console.log('Conectado ao MySQL');
-})
+});
 
 //Criando Rota de Teste
 app.get('/',(req,res)=>{ //Define uma rota na URL http://localhost:5000/
     res.send('API do site está rodando!'); //Se alguém acessar essa rota, o servidor responderá com essa mensagem
-})
+});
 
 //Definição da Porta e Inicialização do Servidor
 const PORT = 5000;
 app.listen(PORT, () =>{ //Inicia o servidor e exibe uma mensagem no console
     console.log(`Servidor rodando na porta ${PORT} `);
-})
+});
 
 
 
