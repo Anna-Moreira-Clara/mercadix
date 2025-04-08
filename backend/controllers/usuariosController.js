4e4c19740aa89b0dd7b6c17bcebcd4d7ef78ae48
Anna Clara Moreira <clarapbi181@gmail.com>
2025-04-08 15:40:00 -0300

    ad

--- backend/controllers/usuariosController.js
+++ backend/controllers/usuariosController.js
@@ -2,5 +2,10 @@ const db = require('../db');
 
 //criar um novo usuário
 exports.criarUsuario = (req,res) =>{
-    const{nome,email,senha} = req.body;
+    const{nome, cpf, email, senha, endereco, telefone, role} = req.body;
+    const sql = 'INSERT INTO usuarios (nome, cpf, email, senha, endereco, telefone,role)VALUES(?,?,?,?,?,?,?)';
+
+    db.query(sql, [nome, cpf, email, senha, endereco, telefone, role], (err,result)=>{
+        if
+    })
 }
