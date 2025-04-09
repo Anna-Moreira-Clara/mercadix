0913f085a68b44748fa5b43e599e1782f8afe2c7
gabriel-marriel <gabrielmsgoncalves@gmail.com>
2025-04-08 21:10:45 -0300

    codico de cadastro

---
+++ src/assets/components/Cadastro/Cadastro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Cadastro() {
+    return (
+      <div className="">
+        <h2>Cadastro</h2>
+        <form>
+          <input type="text" placeholder="Usuário" />
+          <input type="password" placeholder="Senha" />
+          <button type="submit">Cadastrar</button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default Login;
