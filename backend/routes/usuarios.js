58a777ee9477f44a3bfd02e4a768b10cfd44b347
Anna Clara Moreira <clarapbi181@gmail.com>
2025-04-09 15:25:27 -0300

    login usuario

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -5,7 +5,7 @@ const usuariosController = require('../controllers/usuariosController');
 // Rotas de usuários usando o controller
 router.post('/', usuariosController.criarUsuario);
 router.get('/', usuariosController.listarUsuarios);
-router.get('/:id', usuariosController.buscarUsuarioPorId);
+router.post('/:login', usuariosController.loginUsuario);
 router.put('/:id', usuariosController.atualizarUsuario);
 router.delete('/:id', usuariosController.deletarUsuario);
 
