954a2a07d12d62e3eec6af3c5c43f483972241c6
gabriel-marriel <gabrielmsgoncalves@gmail.com>
2025-04-08 21:36:11 -0300

    cadastro

--- src/assets/components/Cadastro/Cadastro.jsx
+++ src/assets/components/Cadastro/Cadastro.jsx
@@ -5,9 +5,11 @@ function Cadastro() {
       <div className="">
         <h2>Cadastro</h2>
         <form>
-          
+          <label>Digite seu Nome:</label>
           <input type="text" placeholder="Nome" />
+          <label>Digite seu Email:</label>
           <input type="text" placeholder="Usuario" />
+          <label>Digite sua Senha:</label>
           <input type="password" placeholder="Senha" />
           <button type="submit">Cadastrar</button>
         </form>
