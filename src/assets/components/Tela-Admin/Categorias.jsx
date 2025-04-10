d08590e95b8733e7e1659d00f35a895b9d91e3d4
Danielle <daniellemariajobs15@gmail.com>
2025-04-10 15:08:16 -0300

    dash

---
+++ src/assets/components/Tela-Admin/Categorias.jsx
@@ -0,0 +1,13 @@
+// Produtos.jsx
+import React from 'react';
+
+const Categorias = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Página de Categorias</h1>
+      <p>Conteúdo da administração de categorias aqui.</p>
+    </div>
+  );
+};
+
+export default Categorias;
