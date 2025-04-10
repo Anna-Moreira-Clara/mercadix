92b75e1bdae1749c0a02c83cfd590c159cdc1f7f
Porfirio27 <vinicius.olivera2012@hotmail.com>
2025-04-10 14:26:32 -0300

    asdasdas

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -164,7 +164,7 @@ const Navbar = () => {
             <nav className="navbar">
                 {usuarioLogado ? (
                     <div className="usuario-logado">
-                        Olá, {usuarioLogado.nome}
+                       <h1> Olá, {usuarioLogado.nome}</h1>
                         <button className="btn sair-btn" onClick={handleLogout}>Sair</button>
                     </div>
                 ) : (
