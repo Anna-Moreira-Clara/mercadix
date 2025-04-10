0ecc25245943ab0a60a4fafb812d916fceb0a5e9
Anna Clara Moreira <clarapbi181@gmail.com>
2025-04-10 15:42:42 -0300

    criei as rotas para carrinho

---
+++ backend/routes/carrinho.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const carrinhoController = require('../controllers/carrinhoController');
+const { MdOutlineElectricMoped } = require('react-icons/md');
+
+router.post('/',carrinhoController.adicionarItem);
+router.get('/:usuario_id', carrinhoController.listarCarrinho);
+router.put('/:id',carrinhoController.atualizarQuantidade);
+router.delete('/:id',carrinhoController.removerItem);
+router.delete('/usuario/:usuario_id', carrinhoController.limparCarrinho);
+
+module.exports = router;
