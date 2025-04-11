dc006629fa9a63594a10cd3a8f114e1e5644c981
gabriel-marriel <gabrielmsg0ncalves@gmail.com>
2025-04-11 16:16:13 -0300

    acougue

--- src/assets/components/Menu-Hamburguer/Acougue.jsx
+++ src/assets/components/Menu-Hamburguer/Acougue.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import picanha from "../Categorias/imagens/picanha.png";
 import linguica from "../Categorias/imagens/linguiça.jpg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const imagens = {
   
     "picanha.png": picanha,
@@ -23,22 +26,26 @@ const imagens = {
         });
     }, []);
   
-    return (
-      <section className="produtos-container">
-        {produtos.map((produto) => (
-          <div key={produto.id} className="produto">
-            <img
-              src={imagens[produto.imagem] || morango} // usa imagem local ou imagem padrão
-              alt={produto.nome}
-              className="imagem-produto"
-            />
-            <p>{produto.nome}</p>
-            <p className="preco">R$ {parseFloat(produto.preco).toFixed(2)}</p>
-            <button className="add-to-cart">Adicionar</button>
-          </div>
-        ))}
-      </section>
-    );
+    const nomesPermitidos = ["Picanha", "Liinguiça"];
+    
+      return (
+        <section className="produtos-container">
+          {produtos
+            .filter((produto) => nomesPermitidos.includes(produto.nome))
+            .map((produto) => (
+              <div key={produto.id} className="produto">
+                <img
+                  src={imagens[produto.imagem] || morango} // usa imagem local ou imagem padrão
+                  alt={produto.nome}
+                  className="imagem-produto"
+                />
+                <p>{produto.nome}</p>
+                <p className="preco">R$ {parseFloat(produto.preco).toFixed(2)}</p>
+                <button className="add-to-cart">Adicionar</button>
+              </div>
+            ))}
+        </section>
+      );
   };
   
   export default Produtos;
