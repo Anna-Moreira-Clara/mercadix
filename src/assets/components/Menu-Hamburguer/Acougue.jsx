2aa3b37ab1fc16e9979521f12bcc041e0db05a4f
Danielle <daniellemariajobs15@gmail.com>
2025-04-11 15:53:26 -0300

    acouguyee

--- src/assets/components/Menu-Hamburguer/Acougue.jsx
+++ src/assets/components/Menu-Hamburguer/Acougue.jsx
@@ -1,3 +1,46 @@
 import React from "react";
-
+import picanha from "../Categorias/imagens/picanha.png";
+import linguica from "../Categorias/imagens/linguiça.jpg";
+const imagens = {
+  
+    "picanha.png": picanha,
+    "linguiça.jpg": linguica,
+   
+   
+  };
+  
+  const Produtos = () => {
+    const [produtos, setProdutos] = useState([]);
+  
+    useEffect(() => {
+      axios
+        .get("http://localhost:5000/produtos")
+        .then((response) => {
+          setProdutos(response.data);
+        })
+        .catch((error) => {
+          console.error("Erro ao buscar produtos:", error);
+        });
+    }, []);
+  
+    return (
+      <section className="produtos-container">
+        {produtos.map((produto) => (
+          <div key={produto.id} className="produto">
+            <img
+              src={imagens[produto.imagem] || morango} // usa imagem local ou imagem padrão
+              alt={produto.nome}
+              className="imagem-produto"
+            />
+            <p>{produto.nome}</p>
+            <p className="preco">R$ {parseFloat(produto.preco).toFixed(2)}</p>
+            <button className="add-to-cart">Adicionar</button>
+          </div>
+        ))}
+      </section>
+    );
+  };
+  
+  export default Produtos;
+  
 
