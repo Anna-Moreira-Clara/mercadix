e7b3910d2b58be170d7d576a3938a559c47a430d
Anna Clara Moreira <clarapbi181@gmail.com>
2025-04-14 15:17:44 -0300

    criei a rota de pedidos

--- backend/controllers/pedidosController.js
+++ backend/controllers/pedidosController.js
@@ -1,9 +1,28 @@
 const db = require('../db'); //conexão com o banco de dados
 
-const criarPedido = (req,res)=>{
-    const{usuario_id} = req.body;
 
-    if(!usuario_id){
-        return res.status(400)
+//criar novo pedido
+const criarPedido = (req, res) => {
+    const { usuario_id } = req.body;
+
+    if (!usuario_id) {
+        return res.status(400).json({ error: 'Usuário é obrigatório' });
+
     }
-}
+    const sql = 'INSERT INTO pedidos(usuario_id, total, status, data_pedido) VALUES (?, 0, "pendente", NOW()';
+    db.query(sql, [usuario_id], (err, result) => {
+        if (err) {
+            console.error('Erro ao criar pedido: ', err);
+            return res.status(500).json({ error: 'Erro ao criar pedido' });
+        }
+        const pedidoId = result.insertId;
+        res.status(201).json({
+            message: 'Pedido criado com sucesso!',
+            pedido_id: pedidoId
+        });
+    });
+};
+
+ module.exports = {
+    criarPedido
+};
