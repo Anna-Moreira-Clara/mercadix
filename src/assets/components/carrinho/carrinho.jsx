db3172cf9eef78644963124d90af08e1f7460565
Anna Clara Moreira <clarapbi181@gmail.com>
2025-04-29 15:19:33 -0300

    Revert "aaas"
    This reverts commit 012be273a42e54aa7ff88ac249a9fee6526abd9c.

--- src/assets/components/carrinho/carrinho.jsx
+++ src/assets/components/carrinho/carrinho.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Carrinho = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Carrinho cfsompleto</h1>
+      <h1 className="text-2xl font-bold">Carrinho completo</h1>
       
     </div>
   );
